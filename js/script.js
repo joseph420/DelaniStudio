c4a826d73cc044bf525bf7d58b54399ff8863dbf
joseph <cr.muturi@gmail.com>
2019-11-04 11:49:39 +0300

    add logo in homepage

--- js/script.js
+++ js/script.js
@@ -13,4 +13,18 @@ $("clickable3").click(function(){
   $(".management-showing")slideToggle();
 });
 
+var key = '1c2089a31a00d28af6cb0e553ed84697-us20';
+
+$("button").click(function(event) {
+  event.preventDefult();
+  var user = document.getElementById('username').value;
+  alert("Dear"+ user + ",we've got your message.Thank you for reaching out to us.");
+  });
+  
+  $("button").on('click', function(){
+    $('form').each(funtion(){
+      this.reset();
+    });
+  });
+
       
